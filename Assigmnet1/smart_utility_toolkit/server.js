3e517183af1131f8ca918e8596b7b4b6afad4ae9
Rakshit <palrakshit7914@gmail.com>
2026-09-06 15:26:12 +0530

    Implement request handling for root, isEven, about, and contact endpoints in server.js

--- Assigmnet1/smart_utility_toolkit/server.js
+++ Assigmnet1/smart_utility_toolkit/server.js
@@ -4,4 +4,21 @@ const PORT = 3000;
 
 const server = https.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    if (req.url === '/') {
+        res.statuscode = 200;
+        res.end('Welcome to the Smart Utility Toolkit!');
+    }else if (req.url === '/isEven'){
+        res.statuscode = 200;
+        res.end('This is the isEven endpoint. You can use this endpoint to check if a number is even or odd.'); 
+    }else if (req.url === '/about'){
+        res.statuscode = 200;
+        res.end("About Page: Built using Node.js core HTTP module."); 
+    }else if (req.url === '/contact'){
+        res.statuscode = 200;
+        res.end("Contact Page: For inquiries, please contact us at palweb@example.com");
+    }else{
+        res.statuscode = 404;
+        res.end('404 Not Found: The requested resource was not found on this server.');
+    }
 })
